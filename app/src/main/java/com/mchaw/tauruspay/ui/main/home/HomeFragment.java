12e6b6385265af9df1620c1df6e90d6861f30b54
li100 <brucelee2019@126.com>
2019-11-04 16:42:28 +0800

    主页UI

--- app/src/main/java/com/mchaw/tauruspay/ui/main/home/HomeFragment.java
+++ app/src/main/java/com/mchaw/tauruspay/ui/main/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.mchaw.tauruspay.ui.main.home;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -9,12 +11,26 @@ import com.mchaw.tauruspay.base.fragment.BaseFragment;
 import com.mchaw.tauruspay.base.fragment.BasePresentFragment;
 import com.mchaw.tauruspay.di.component.ActivityComponent;
 
+import butterknife.BindView;
+
 /**
  * @author : Bruce Lee
  * @date : 2019/11/3 0003 21:02
  * @description :
  */
 public class HomeFragment extends BaseFragment {
+
+    @BindView(R.id.tv_notice_text)
+    TextView tvNotiveText;
+
+    @BindView(R.id.tv_pre_sale_txt)
+    TextView tvPreSaleTxt;
+
+    @BindView(R.id.tv_after_sale_txt)
+    TextView tvAfterSaleTxt;
+
+    private String strPre = "*开始代售前，请保持<font color='#8d64cb'>金牛话费</font>与<font color='#ff0000'>支付宝</font>在线";
+    private String strAfter = "*开始代售时，请及时查询确认收款";
 
     @Override
     protected int getContentViewId() {
@@ -29,6 +45,8 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initFragment() {
-
+        tvNotiveText.setSelected(true);
+        tvPreSaleTxt.setText(Html.fromHtml(strPre));
+        tvAfterSaleTxt.setText(strAfter);
     }
 }
